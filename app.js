6b53be34738102d1e6db00434258c7b2b4f2d95d
lvillacin <profiles.lvillacin@gmail.com>
2022-03-05 05:09:49 +0800

    logger and globalLogger

--- app.js
+++ app.js
@@ -5,9 +5,11 @@ const cookieParser = require('cookie-parser')
 const cors = require('cors')
 const compression = require('compression')
 const responseTime = require('response-time')
+const globalLogger = require('@middlewares/globalLogger')
 
 //utils imports
 const createError = require('http-errors')
+const logger = require('@utils/logger')
 
 //routers imports
 const indexRouter = require('@routes/index')
@@ -21,8 +23,11 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(responseTime())
+app.use(globalLogger())
 
 //routers
 app.use('/', indexRouter);
+
+logger.info(`RUNNING ${process.env.npm_package_name} v${process.env.npm_package_version} on ${process.env.PORT}`)
 
 module.exports = app;
