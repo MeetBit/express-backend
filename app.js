5e0ba42378d6062adab6a0a90e35bae592077256
lvillacin <profiles.lvillacin@gmail.com>
2022-03-05 03:25:01 +0800

    cleanup

--- app.js
+++ app.js
@@ -1,41 +1,29 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express')
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+//middleware
+const cookieParser = require('cookie-parser')
+const cors = require('cors')
+const compression = require('compression')
+const responseTime = require('response-time')
 
-var app = express();
+//utils
+const createError = require('http-errors')
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+//routers
+const indexRouter = require('./routes/index')
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+const app = express();
 
+//middleware
+app.use(compression())
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(responseTime())
+
+//routers
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
 
 module.exports = app;
